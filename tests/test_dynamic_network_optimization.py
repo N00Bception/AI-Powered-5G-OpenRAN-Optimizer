dfb3ddd83419122a4c0dbf57154f0e00d758d14b
Salim El Ghalbzouri <91485823+N00Bception@users.noreply.github.com>
2023-04-28 22:33:52 +0000

    Update test_dynamic_network_optimization.py

--- tests/test_dynamic_network_optimization.py
+++ tests/test_dynamic_network_optimization.py
@@ -1,1 +1,57 @@
+import unittest
+import numpy as np
+from src.models.dynamic_network_optimization import DynamicNetworkOptimization
+
+class TestDynamicNetworkOptimization(unittest.TestCase):
+    
+    def setUp(self):
+        # Initialize test data
+        self.network = np.array([
+            [0, 10, 15, 20],
+            [10, 0, 35, 25],
+            [15, 35, 0, 30],
+            [20, 25, 30, 0]
+        ])
+        self.demand = np.array([
+            [0, 200, 300, 100],
+            [200, 0, 100, 300],
+            [300, 100, 0, 200],
+            [100, 300, 200, 0]
+        ])
+    
+    def test_constructor(self):
+        # Test if object is initialized with the correct attributes
+        dno = DynamicNetworkOptimization(self.network, self.demand)
+        self.assertTrue((dno.network == self.network).all())
+        self.assertTrue((dno.demand == self.demand).all())
+        self.assertEqual(dno.num_nodes, len(self.network))
+    
+    def test_optimal_flow(self):
+        # Test if the optimal flow is calculated correctly
+        dno = DynamicNetworkOptimization(self.network, self.demand)
+        flow = dno.optimal_flow()
+        self.assertEqual(flow.sum(), self.demand.sum())
+        # Add more specific tests based on known solutions or theoretical bounds
+    
+    def test_infeasible_demand(self):
+        # Test if the function raises an error when demand is infeasible
+        dno = DynamicNetworkOptimization(self.network, self.demand + 1000)
+        with self.assertRaises(ValueError):
+            dno.optimal_flow()
+    
+    def test_negative_flow(self):
+        # Test if the function raises an error when negative flow is generated
+        dno = DynamicNetworkOptimization(self.network, self.demand)
+        dno.network[0, 1] = -10
+        with self.assertRaises(ValueError):
+            dno.optimal_flow()
+    
+    def test_empty_network(self):
+        # Test if the function raises an error when the network is empty
+        dno = DynamicNetworkOptimization(np.array([]), self.demand)
+        with self.assertRaises(ValueError):
+            dno.optimal_flow()
+
+if __name__ == '__main__':
+    unittest.main()
 
