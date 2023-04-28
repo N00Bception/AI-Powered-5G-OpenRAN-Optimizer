d969de6feb41a43eb32492557e214d5c967d5174
xxSaleim <saleim.elg@gmail.com>
2023-04-28 18:03:29 +0100

    update model.py

--- src/models/energy_efficiency_optimization/model.py
+++ src/models/energy_efficiency_optimization/model.py
@@ -1,1 +1,19 @@
+import torch.nn as nn
+
+class EnergyEfficiencyModel(nn.Module):
+    """
+    PyTorch model for energy efficiency optimization in 5G OpenRAN.
+    """
+    
+    def __init__(self, input_size, hidden_size, output_size):
+        super(EnergyEfficiencyModel, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
 
