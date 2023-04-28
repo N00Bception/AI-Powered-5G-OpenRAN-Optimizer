3740f2ebf17ec44b30c742be58d10be86be6f2d1
xxSaleim <saleim.elg@gmail.com>
2023-04-28 18:05:58 +0100

    Update predict.py

--- src/models/energy_efficiency_optimization/predict.py
+++ src/models/energy_efficiency_optimization/predict.py
@@ -1,1 +1,53 @@
+import argparse
+import os
+import sys
+import pandas as pd
+import torch
+
+from utils import load_data, preprocess_data, load_model
+
+
+def main(args):
+    # Load the data
+    data_path = os.path.join(args.data_dir, args.data_file)
+    data = load_data(data_path)
+
+    # Preprocess the data
+    data = preprocess_data(data)
+
+    # Load the model
+    model_path = os.path.join(args.model_dir, args.model_file)
+    model = load_model(model_path)
+
+    # Make predictions
+    with torch.no_grad():
+        inputs = torch.tensor(data.values).float()
+        predictions = model(inputs).squeeze().tolist()
+
+    # Save predictions to file
+    output_path = os.path.join(args.output_dir, args.output_file)
+    with open(output_path, 'w') as f:
+        for pred in predictions:
+            f.write(str(pred) + '\n')
+
+    print(f"Predictions saved to {output_path}.")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # Data arguments
+    parser.add_argument('--data_dir', type=str, default='data', help="Directory containing data file.")
+    parser.add_argument('--data_file', type=str, default='test.csv', help="Data file name.")
+
+    # Model arguments
+    parser.add_argument('--model_dir', type=str, default='models/energy_efficiency_optimization', help="Directory containing model file.")
+    parser.add_argument('--model_file', type=str, default='model.pt', help="Model file name.")
+
+    # Output arguments
+    parser.add_argument('--output_dir', type=str, default='output', help="Directory to save predictions file.")
+    parser.add_argument('--output_file', type=str, default='predictions.txt', help="Predictions file name.")
+
+    args = parser.parse_args()
+    main(args)
 
