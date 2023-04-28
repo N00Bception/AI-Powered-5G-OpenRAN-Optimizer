386e1c0731355286a5f35ee2dae14738539da8ea
Salim El Ghalbzouri <91485823+N00Bception@users.noreply.github.com>
2023-04-28 18:18:56 +0000

    Update data_preprocessing.py

--- src/utils/data_preprocessing.py
+++ src/utils/data_preprocessing.py
@@ -1,1 +1,27 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+
+def preprocess_data(data_file_path: str, scaler_type: str = 'min_max') -> pd.DataFrame:
+    # Load data
+    data = pd.read_csv(data_file_path)
+
+    # Drop irrelevant columns
+    data.drop(['id', 'timestamp'], axis=1, inplace=True)
+
+    # Apply scaling
+    if scaler_type == 'min_max':
+        scaler = MinMaxScaler()
+    elif scaler_type == 'standard':
+        scaler = StandardScaler()
+    else:
+        raise ValueError(f"Unknown scaler type: {scaler_type}")
+    
+    data.iloc[:, :-1] = scaler.fit_transform(data.iloc[:, :-1])
+
+    # Split into input and target
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
+
+    return X, y
 
