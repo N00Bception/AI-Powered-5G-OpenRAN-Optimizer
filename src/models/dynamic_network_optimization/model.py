34dac9091e6942c50979cd15b646db2f60c05f1c
xxSaleim <saleim.elg@gmail.com>
2023-04-28 17:53:54 +0100

    Update model.py

--- src/models/dynamic_network_optimization/model.py
+++ src/models/dynamic_network_optimization/model.py
@@ -1,1 +1,28 @@
+import tensorflow as tf
+
+class DynamicNetworkOptimizationModel(tf.keras.Model):
+    def __init__(self):
+        super(DynamicNetworkOptimizationModel, self).__init__()
+        
+        # Define layers and architecture of the model
+        self.conv1 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu')
+        self.pool1 = tf.keras.layers.MaxPooling2D((2, 2))
+        self.conv2 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')
+        self.pool2 = tf.keras.layers.MaxPooling2D((2, 2))
+        self.flatten = tf.keras.layers.Flatten()
+        self.dense1 = tf.keras.layers.Dense(128, activation='relu')
+        self.dropout = tf.keras.layers.Dropout(0.2)
+        self.dense2 = tf.keras.layers.Dense(1, activation='sigmoid')
+
+    def call(self, inputs):
+        # Define the forward pass of the model
+        x = self.conv1(inputs)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = self.flatten(x)
+        x = self.dense1(x)
+        x = self.dropout(x)
+        output = self.dense2(x)
+        return output
 
