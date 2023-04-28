e2582a0e3bb80281aac002ca7e685843b2321a14
Salim El Ghalbzouri <91485823+N00Bception@users.noreply.github.com>
2023-04-28 17:20:07 +0000

    Update predict.py

--- src/models/predictive_network_planning/predict.py
+++ src/models/predictive_network_planning/predict.py
@@ -1,1 +1,60 @@
+import argparse
+import logging
+import os
+
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.preprocessing import MinMaxScaler
+from torch.utils.data import DataLoader
+
+from models.predictive_network_planning.model import PredictiveNetworkPlanningModel
+from models.predictive_network_planning.utils import PNPDataset, collate_fn
+
+
+def predict(model: torch.nn.Module, data: PNPDataset, scaler: MinMaxScaler, device: str) -> np.ndarray:
+    loader = DataLoader(data, batch_size=1, collate_fn=collate_fn)
+    outputs = []
+    model.eval()
+    with torch.no_grad():
+        for batch in loader:
+            inputs, targets = batch
+            inputs, targets = inputs.to(device), targets.to(device)
+            output = model(inputs)
+            outputs.append(output.item())
+    outputs = np.array(outputs).reshape(-1, 1)
+    outputs = scaler.inverse_transform(outputs)
+    return outputs
+
+
+def main(args: argparse.Namespace) -> None:
+    # Load model
+    model = PredictiveNetworkPlanningModel(input_dim=args.input_dim,
+                                           hidden_dim=args.hidden_dim,
+                                           num_layers=args.num_layers,
+                                           output_dim=args.output_dim,
+                                           dropout=args.dropout)
+    device = torch.device(args.device)
+    model.load_state_dict(torch.load(args.model_path, map_location=device))
+    model.to(device)
+
+    # Load data and scaler
+    data = pd.read_csv(args.data_file)
+    scaler = MinMaxScaler()
+    scaler.fit(data.values)
+
+    # Convert data to PyTorch dataset
+    data = PNPDataset(data_file=args.data_file)
+    inputs, targets = data[0]
+    input_dim = inputs.shape[-1]
+    output_dim = targets.shape[-1]
+
+    # Make prediction
+    prediction = predict(model, data, scaler, device)
+    logging.info(f"Input Dimension: {input_dim}, Output Dimension: {output_dim}")
+    logging.info(f"Prediction: {prediction}")
+
+    # Save prediction
+    os.makedirs(args.output_dir, exist_ok=True)
+    np.savetxt(os.path.join(args.output_dir, args.output_file), prediction, delimiter=",")
 
