065ab6e6b489ea92a8eefa80701f34354ea4d242
Salim El Ghalbzouri <91485823+N00Bception@users.noreply.github.com>
2023-04-28 18:20:20 +0000

    Update model_prediction.py

--- src/utils/model_prediction.py
+++ src/utils/model_prediction.py
@@ -1,1 +1,45 @@
+from typing import List, Dict
+import numpy as np
+import tensorflow as tf
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+def predict(model: tf.keras.models.Model, data: np.ndarray) -> np.ndarray:
+    """
+    Predicts the output of the model for the given input data.
+
+    Args:
+        model: A trained TensorFlow Keras model.
+        data: A 3D numpy array of shape (num_samples, timesteps, features) containing the input data.
+
+    Returns:
+        A 2D numpy array of shape (num_samples, output_dim) containing the model's predictions.
+    """
+    logger.info("Starting model prediction...")
+    predictions = model.predict(data)
+    logger.info("Prediction completed!")
+    return predictions
+
+
+def postprocess(predictions: np.ndarray, output_dim: int) -> List[Dict[str, float]]:
+    """
+    Post-processes the predictions and converts them to a list of dictionaries.
+
+    Args:
+        predictions: A 2D numpy array of shape (num_samples, output_dim) containing the model's predictions.
+        output_dim: An integer indicating the number of output dimensions.
+
+    Returns:
+        A list of dictionaries, where each dictionary contains the predicted values for each output dimension.
+    """
+    logger.info("Starting post-processing...")
+    prediction_list = []
+    for i in range(predictions.shape[0]):
+        prediction_dict = {}
+        for j in range(output_dim):
+            prediction_dict[f"output_{j}"] = float(predictions[i][j])
+        prediction_list.append(prediction_dict)
+    logger.info("Post-processing completed!")
+    return prediction_list
 
