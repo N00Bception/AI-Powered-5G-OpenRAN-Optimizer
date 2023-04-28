91554c228fa8f857bde3c5cec1bd55fd70359b37
Salim El Ghalbzouri <91485823+N00Bception@users.noreply.github.com>
2023-04-28 18:17:53 +0000

    Update config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,1 +1,16 @@
+import yaml
 
+class Config:
+    def __init__(self, config_file):
+        with open(config_file, "r") as f:
+            self._config = yaml.safe_load(f)
+
+    def get(self, key, default=None):
+        return self._config.get(key, default)
+
+    def set(self, key, value):
+        self._config[key] = value
+
+    def save(self, config_file):
+        with open(config_file, "w") as f:
+            yaml.safe_dump(self._config, f, default_flow_style=False)
