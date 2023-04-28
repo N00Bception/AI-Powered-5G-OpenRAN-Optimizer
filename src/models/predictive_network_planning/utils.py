00761398e44460017c6e1fa463938dbc3323a73a
Salim El Ghalbzouri <91485823+N00Bception@users.noreply.github.com>
2023-04-28 17:09:20 +0000

    Update utils.py

--- src/models/predictive_network_planning/utils.py
+++ src/models/predictive_network_planning/utils.py
@@ -1,1 +1,37 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+def load_data(data_path):
+    """
+    Load data from csv file.
+
+    Args:
+        data_path (str): Path to the csv file.
+
+    Returns:
+        X (numpy array): Features of the dataset.
+        y (numpy array): Labels of the dataset.
+    """
+    df = pd.read_csv(data_path)
+    X = df.iloc[:, :-1].values
+    y = df.iloc[:, -1].values
+    return X, y
+
+def preprocess_data(X_train, X_test):
+    """
+    Preprocess data using standard scaling.
+
+    Args:
+        X_train (numpy array): Features of the training set.
+        X_test (numpy array): Features of the test set.
+
+    Returns:
+        X_train_scaled (numpy array): Scaled features of the training set.
+        X_test_scaled (numpy array): Scaled features of the test set.
+    """
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    return X_train_scaled, X_test_scaled
 
