02715e0024ac5b90896efa2e76dfd0ab405cfe10
Salim El Ghalbzouri <91485823+N00Bception@users.noreply.github.com>
2023-04-28 17:09:02 +0000

    Update model.py

--- src/models/predictive_network_planning/model.py
+++ src/models/predictive_network_planning/model.py
@@ -1,1 +1,19 @@
+import torch
+import torch.nn as nn
+
+class PredictiveNetwork(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
+        
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        return out
 
