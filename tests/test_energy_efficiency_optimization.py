b1ee61ffbab4599249c8470bcaa6ddbebb1bfaab
Salim El Ghalbzouri <91485823+N00Bception@users.noreply.github.com>
2023-04-30 11:41:45 +0100

    Update test_energy_efficiency_optimization.py

--- tests/test_energy_efficiency_optimization.py
+++ tests/test_energy_efficiency_optimization.py
@@ -1,1 +1,55 @@
+import unittest
+from src.utils.config import Config
+from src.utils.logger import Logger
+from src.data_preparation.data_loader import DataLoader
+from src.data_preparation.data_cleaning import DataCleaning
+from src.data_preparation.data_extraction import DataExtraction
+from src.data_preparation.data_transformation import DataTransformation
+from src.dynamic_network_optimization.energy_efficiency_optimization import EnergyEfficiencyOptimization
+
+class TestEnergyEfficiencyOptimization(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        cls.config = Config()
+        cls.logger = Logger()
+        cls.data_loader = DataLoader(cls.config, cls.logger)
+        cls.data_cleaning = DataCleaning(cls.config, cls.logger)
+        cls.data_extraction = DataExtraction(cls.config, cls.logger)
+        cls.data_transformation = DataTransformation(cls.config, cls.logger)
+        cls.eeo = EnergyEfficiencyOptimization(cls.config, cls.logger)
+
+    def test_energy_efficiency_optimization(self):
+        # Load test data
+        test_data = self.data_loader.load_test_data("test_data.csv")
+        
+        # Clean test data
+        test_data = self.data_cleaning.clean_test_data(test_data)
+        
+        # Extract relevant features from test data
+        test_data = self.data_extraction.extract_test_data(test_data)
+        
+        # Transform test data
+        test_data = self.data_transformation.transform_test_data(test_data)
+        
+        # Run energy efficiency optimization
+        result = self.eeo.run(test_data)
+        
+        # Assert that result is not empty
+        self.assertIsNotNone(result)
+        
+        # Assert that result is a dictionary
+        self.assertIsInstance(result, dict)
+        
+        # Assert that result contains expected keys
+        self.assertSetEqual(set(result.keys()), {"nodes", "edges", "cost"})
+        
+        # Assert that result values are of correct type and format
+        self.assertIsInstance(result["nodes"], list)
+        self.assertIsInstance(result["edges"], list)
+        self.assertIsInstance(result["cost"], float)
+        self.assertGreater(result["cost"], 0.0)
+        
+if __name__ == "__main__":
+    unittest.main()
 
